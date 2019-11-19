17a835bc68dafa5bcc6ddd25a829c9c13fd48fd6
Sung Chan Hwang <34934883+plzprayme@users.noreply.github.com>
2019-11-19 16:10:34 +0900

    no -> id

--- models/dbconnection.js
+++ models/dbconnection.js
@@ -1,16 +1,17 @@
-let mysql = require('mysql')
+// pooling connection
 
-let pool = mysql.createPool({
+var mysql = require('mysql');
+var pool = mysql.createPool({
     connectionLimit: 10,
     host: 'localhost',
     user: 'root',
-    password: '1234',
+    password: '3357ppay586!',
     database: 'board'
-})
+});
 
 pool.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
     if (error) throw error;
     console.log('The solution is: ', results[0].solution);
   });
 
-  module.exports = pool
+module.exports = pool
--- routes/index.js
+++ routes/index.js
@@ -1,153 +1,187 @@
 var express = require('express');
 var router = express.Router();
-let pool = require('../models/dbconnection')
+var pool = require('../models/dbconnection')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-
-  sql = "SELECT `id`, `title`, `author`, DATE_FORMAT(regdate, '%Y\/%c\/%e') AS regdate2, `hit` FROM board.board;"
+router.get('/', function (req, res, next) {
+  var sql = "SELECT `id`, `title`, `author`, DATE_FORMAT(regdate, '%Y\/%c\/%e') AS regdate2, `hit` FROM board.board;";
   pool.getConnection((err, conn) => {
     if (err) return next(err)
-    conn.query( sql, [], (err,docs) => {
-      if(err) return next(err)
+    conn.query(sql, [], (err, docs) => {
+      if (err) return next(err)
+      console.log(docs)
       var data = {
-        "rows" : docs,
-        "title" : "연습용 게시판"
+        "rows": docs,
+        "title": "연습용 게시판"
+
       }
-      console.log(data)
-      conn.release() // DB를 다 썼으면 반납해야지?
+      conn.release() // DB 다 쓰면 반납
       res.render('index', data);
     })
   })
 });
 
-router.get('/read/:id', (req, res, next) => {
-  let id = req.params.id
-  
-  pool.getConnection((err, conn) => {
-    if(err) return next(err)
-    sql = "SELECT `id`, `title`, `author`, DATE_FORMAT(regdate, '%Y\/%c\/%e') AS regdate2,`content`, `hit` FROM board.board WHERE id = ?;"
-
-    conn.query(sql, [id], (err, row) => {
-      if(err) return next(err)
-      // console.log(row[0])
-      let data = {
-        "rows" : row[0],
-        "title" : "게시판 읽기"
-      }
-      conn.release()
-      res.render('read', data)
-    })
-  })
-})
-
-router.get('/updateform/:id', (req, res, next) => {
-  let id = req.params.id
-
-  pool.getConnection((err, conn) => {
-    if(err) return next(err)
-    sql = "SELECT `id`, `title`, `author`, DATE_FORMAT(regdate, '%Y\/%c\/%e') AS regdate2,`content`, `hit` FROM board.board WHERE id = ?;"
-
-    conn.query(sql, [id], (err, row) => {
-      if(err) return next(err)
-      
-      let data = {
-        "rows" : row[0],
-        "title" : "게시판 읽기"
-      }
-      conn.release()
-      res.render('updateform', data)
-    })
-  })
-  
-})
-
-router.post('/update', (req,res,next) => {
-  
-  let id = req.body.id
-  let title = req.body.title
-  let author = req.body.author
-  let pw = req.body.pw
-  let content = req.body.content
-
-  pool.getConnection((err, conn) => {
-    let sql = "UPDATE board SET `title` = ?, `author`=?, `content`=?, `regdate`=now() WHERE `id`=? AND `pw`=?"
-    let arr = [title, author, content, id, pw]
-
-    if(err) return next(err)
-    else {
-      conn.query(sql, arr, (err,row) => {
-        if(err) return next(err)
-        else {
-          if(row.affectedRows == 1) res.redirect('/')
-          else res.send("<script>alert('비밀번호가 틀려서 되돌아 갑니다.'); history.back(); </script>")
-        }
-        conn.release()
-      })
-    }
-
-    
-  })
-
-})
-
 router.get('/writeform', (req, res, next) => {
-  res.render('writeform', { title: '연습장'})
+  res.render('writeform', { title: '연습장' })
 })
 
 router.post('/write', (req, res, next) => {
-  let title = req.body.title
-  let author = req.body.author
-  let pw = req.body.pw
-  let content = req.body.content
+  // 받아서 insert 후에 list로 돌아간다.
+  var title = req.body.title
+  var author = req.body.author
+  var pw = req.body.pw
+  var content = req.body.content
+  // console.log("제목 :", title)
+  // console.log("글쓴이 :", author)
+  // console.log("비밀번호 :", pw)
+  // console.log("내용 :", content)
 
-  arr = [title, author, pw, content]
+  //board 테이블에 insert하기
 
-  let sql = 'INSERT INTO board (title, author, regdate, pw, content, hit) VALUES (?,?,now(),?,?,0)'
-
-  pool.getConnection((err, conn) =>{
+  var sql = 'INSERT INTO board(title, author, regdate, pw, content, hit) VALUES (?,?,now(),?,?,0)'
+  var arr = [title, author, pw, content]
+  // console.log(arr)
+  pool.getConnection((err, conn) => {
     if (err) {
       return next(err)
-    } else {
-      conn.query(sql, arr, (err,row) =>{
-        if(err){
+    }
+    else {
+      conn.query(sql, arr, (err, row) => {
+        if (err) {
           return next(err)
-        } else {
-          console.log('row = ', row)
-          
+        }
+        else {
+          console.log('row =', row)
+
           res.redirect('/')
         } conn.release()
       })
     }
   })
+
 })
 
-router.get('/deleteform/:id', (req,res) => {
-  let id = req.params.id
-  let data = {
-    "id" : id,
+router.get('/read/:no', (req, res, next) => {
+  var no = req.params.no
+  // console.log("입력한 숫자는?? == ",no)
+
+  pool.getConnection((err, conn) => {
+    if (err) return next(err)
+    sql = "SELECT `no`, `title`, `author`, DATE_FORMAT(regdate, '%Y\/%c\/%e') AS regdate2, `content`, `hit` FROM board.board WHERE no = ?;"
+
+    conn.query("UPDATE board SET `hit` = `hit` + 1 WHERE `no` = ?", [no], (err, doc) => {
+      if (err) {
+        return next(err);
+      }
+      conn.query(sql, [no], (err, row) => {
+        if (err) return next(err)
+        console.log(row[0])
+        var data = {
+          "rows": row[0],
+          "title": "게시판 읽기"
+        }
+        conn.release() // DB 다 쓰면 반납
+        res.render('read', data)
+      });
+    })
+  })
+})
+
+router.get('/updateform/:no', (req, res, next) => {
+  var no = req.params.no
+  // console.log("입력한 숫자는?? == ",no)
+
+  pool.getConnection((err, conn) => {
+    if (err) return next(err)
+    sql = "SELECT `no`, `title`, `author`, DATE_FORMAT(regdate, '%Y\/%c\/%e') AS regdate2, `content`, `hit`,`pw` FROM board.board WHERE no = ?;";
+
+    conn.query(sql, [no], (err, row) => {
+      if (err) {
+        return next(err)
+      }
+      else {
+        console.log("updateform", row[0])
+        var data = {
+          "rows": row[0],
+          "title": "게시판 읽기"
+        }
+      }
+      conn.release() // DB 다 쓰면 반납
+      res.render('updateform', data)
+    });
+  })
+})
+
+router.post('/update', (req, res, err) => {
+
+  var no = req.body.no
+  var title = req.body.title
+  var author = req.body.author
+  var pw = req.body.pw
+  var content = req.body.content
+
+  // console.log("제목 :", title)
+  // console.log("글쓴이 :", author)
+  // console.log("비밀번호 :", pw)
+  // console.log("내용 :", content)
+
+  //board 테이블에 insert하기
+
+  var sql = "UPDATE board SET `title` =?, `author` =?, `content` =?, `regdate`=now() WHERE `no` =? AND `pw` =?";
+  var arr = [title, author, content, no, pw];
+  // console.log(arr)
+  pool.getConnection((err, conn) => {
+    if (err) {
+      return next(err)
+    }
+    else {
+      conn.query(sql, arr, (err, row) => {
+        if (err) {
+          console.log(err);
+          return next(err);
+        }
+        else {
+          console.log('row =', row);
+          if(row.affectedRows == 1) {
+            res.redirect('/')
+          } else {
+            res.send("<script>alert('비밀번호가 틀려서 되돌아 갑니다.'); hitory.back(); </script>")
+          }
+          conn.release();
+        } 
+      })
+    }
+  })
+
+})
+
+router.get('/deleteform/:no', (req, res) => {
+  var no = req.params.no
+  console.log("no = ", no)
+  var data = {
+    "no" : no,
     "title" : "게시글 삭제"
   }
-  res.render('deleteform', data)
+  res.render('deleteform' , data)
 })
 
-router.post('/delete',(req, res, next) => {
-
-  let id = req.body.id
-  let pw = req.body.pw
+router.post('/delete', (req,res, next) => {
+  console.log("delete req body", req.body)
+  var no = req.body.no
+  var pw = req.body.pw
   
-  pool.getConnection((err, conn) => { 
-  if (err) return next(err)
-  else{
-    let sql = 'DELETE FROM board WHERE id=? AND pw=?'
-    conn.query(sql, [id, pw], (err,row) =>{
-      console.log('update row = ', row)
-      if (row.affectedRows == 1)
+  pool.getConnection((err, conn) => {
+    if(err) { return next(err) }
+    var sql = 'DELETE FROM board WHERE no=? AND pw=?'
+    conn.query(sql, [no, pw], (err, row) => {
+      console.log('update row =', row)
+      if (row.affectedRows == 1){
         res.redirect('/')
-      else
-        res.send('<script> alert("비밀번호가 틀려서 되돌아 갑니다."); history.back(); </script>')
+      } else {
+        res.send('<script> alert("비밀번호가 틀려서 되돌아 갑니다."); history.back(); </script>') 
+      }
     })
-  }})
+  })
 })
 
 module.exports = router;
